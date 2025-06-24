8e34f830f554891066b926b6d9e41b05adfbb65d
Bushra Rahman <rbushra402@gmail.com>
2025-06-24 15:47:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-
+import random
+def greet():
+    greetings = ["Hi!","Hello!","Hey!","Hi there!","What's up?"]
+    return random.choice(greetings)
+def maths(user_input):
+    try:
+        ques=user_input.replace("calculate","").replace("what is","").strip()
+        ans=eval(ques)
+        return f"the answer is {ans}"
+    except:
+        return "That doesn't look like maths..."
+def chatbot():
+    return
+running=True
+print("Bot: " + greet())
+while running:
+    user_input=input("You: ")
+    if "calculate" in user_input.lower():
+        print("Bot:", maths(user_input))
+    if "exit" in user_input.lower() or "quit" in user_input.lower():
+        print("Bot: Goodbye!")
+        running=False
+        
